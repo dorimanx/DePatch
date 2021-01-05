8702929cd218b0752d7c7dcbdf23965587bbf8bf
LTP <vinduus585@gmail.com>
2021-01-05 12:50:13 +0000

    tried fix enemy building  in pve zones
    i seen player stole items from enemy grid in pve zone by building conveyor sorter

---
+++ DePatch/CubeGridExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Sandbox.Game.Entities;
+using Sandbox.Game.World;
+using Torch;
+using VRage.Game;
+
+namespace DePatch
+{
+    public static class CubeGridExtensions
+    {
+        public static bool IsFriendlyPlayer(this MyCubeGrid grid, ulong steamId)
+        {
+            var player = MySession.Static.Players.TryGetPlayerBySteamId(steamId);
+            if (player == null) return false;
+            var owner = grid.BigOwners.FirstOrDefault();
+            if (owner == default) return true;
+            MySession.Static.Players.TryGetPlayerId(owner, out var ownerId);
+            var ownerPlayer = MySession.Static.Players.GetPlayerById(ownerId);
+            var relation = ownerPlayer.GetRelationTo(player.Identity.IdentityId);
+            return relation.IsFriendly();
+        }
+    }
+}
--- DePatch/PVEZONE/MyNewGridPatch.cs
+++ DePatch/PVEZONE/MyNewGridPatch.cs
@@ -9,38 +9,32 @@ namespace DePatch.PVEZONE
         private static void Postfix(MyCubeGrid __instance)
         {
             var cubegrid = __instance;
-            if (cubegrid != null)
+            if (cubegrid == null) return;
+            if (!DePatchPlugin.GameIsReady || !DePatchPlugin.Instance.Config.Enabled) return;
+            if (DePatchPlugin.Instance.Config.PveZoneEnabled)
             {
-                if (DePatchPlugin.GameIsReady && DePatchPlugin.Instance.Config.Enabled)
+                if (!PVEGrid.Grids.ContainsKey(cubegrid))
                 {
-                    if (DePatchPlugin.Instance.Config.PveZoneEnabled)
-                    {
-                        if (!PVEGrid.Grids.ContainsKey(cubegrid))
-                        {
-                            PVEGrid.Grids?.Add(cubegrid, new PVEGrid(cubegrid));
-                        }
-                        var pVEGrid = PVEGrid.Grids[cubegrid];
-                        if (pVEGrid.InPVEZone())
-                        {
-                            PVE.EntitiesInZone.Add(cubegrid.EntityId);
-                            pVEGrid?.OnGridEntered();
-                        }
-                    }
-
-                    if (DePatchPlugin.Instance.Config.PveZoneEnabled2)
-                    {
-                        if (!PVEGrid2.Grids2.ContainsKey(cubegrid))
-                        {
-                            PVEGrid2.Grids2?.Add(cubegrid, new PVEGrid2(cubegrid));
-                        }
-                        var pVEGrid2 = PVEGrid2.Grids2[cubegrid];
-                        if (pVEGrid2.InPVEZone2())
-                        {
-                            PVE.EntitiesInZone2.Add(cubegrid.EntityId);
-                            pVEGrid2?.OnGridEntered2();
-                        }
-                    }
+                    PVEGrid.Grids?.Add(cubegrid, new PVEGrid(cubegrid));
                 }
+                var pVEGrid = PVEGrid.Grids[cubegrid];
+                if (pVEGrid.InPVEZone())
+                {
+                    PVE.EntitiesInZone.Add(cubegrid.EntityId);
+                    pVEGrid?.OnGridEntered();
+                }
+            }
+
+            if (!DePatchPlugin.Instance.Config.PveZoneEnabled2) return;
+            if (!PVEGrid2.Grids2.ContainsKey(cubegrid))
+            {
+                PVEGrid2.Grids2?.Add(cubegrid, new PVEGrid2(cubegrid));
+            }
+            var pVEGrid2 = PVEGrid2.Grids2[cubegrid];
+            if (pVEGrid2.InPVEZone2())
+            {
+                PVE.EntitiesInZone2.Add(cubegrid.EntityId);
+                pVEGrid2?.OnGridEntered2();
             }
         }
     }
