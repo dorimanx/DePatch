5e84284c424123a5fdd11f48555a02b92ac28fc5
Yuri <Yuri@bynet.co.il>
2021-03-04 12:59:26 +0300

    Check MODS update time vs Steam Mods. on start.

---
+++ DePatch/GamePatches/ModUpdatePatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using VRage.GameServices;
+
+namespace DePatch.GamePatches
+{
+    [HarmonyPatch(typeof(MyWorkshopItem))]
+    [HarmonyPatch("IsUpToDate")] 
+    class ModUpdatePatch
+    {
+        static void Postfix(MyWorkshopItem __instance, ref bool __result)
+        {
+            if (__instance.TimeUpdated > __instance.LocalTimeUpdated)
+            {
+                __result = false;
+            }
+        }
+    }
+}
