2d57101a49b17441191d151906c319aa58ea0a7f
Yuri <Yuri@bynet.co.il>
2021-07-14 16:15:20 +0300

    Reworked PlayerIDSave.
    * This was running during worldSave and had chance to trigger silent torch crash on save!
    No more! now will work only on client connect to sync his info to ram.

--- DePatch/GamePatches/MyPlayerIdFix.cs
+++ DePatch/GamePatches/MyPlayerIdFix.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using HarmonyLib;
 using NLog;
 using Sandbox.Game.World;
 using Torch.Managers.PatchManager;
@@ -10,40 +9,29 @@ using VRage.Game;
 
 namespace DePatch.GamePatches
 {
-    //[HarmonyPatch(typeof(MySession), "GetCheckpoint")]
     [PatchShim]
 
     internal static class MyPlayerIdFix
     {
         private static readonly ILogger Log = LogManager.GetCurrentClassLogger();
-        private readonly static int SaveClientsCooldown = 120; //seconds
-        private readonly static long TimerId = 123456789;
         public static readonly Dictionary<long, DateTime> cooldowns = new Dictionary<long, DateTime>();
 
         private static void Patch(PatchContext ctx)
         {
             ctx.GetPattern(typeof(MySession).GetMethod("GetCheckpoint", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)).
-                Suffixes.Add(typeof(MyPlayerIdFix).GetMethod(nameof(GetCheckpoint), BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static));
+                Suffixes.Add(typeof(MyPlayerIdFix).GetMethod(nameof(GetCheckpointPostfix), BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static));
         }
 
-        private static void GetCheckpoint(MyObjectBuilder_Checkpoint __result)
+        private static void GetCheckpointPostfix(MyObjectBuilder_Checkpoint __result)
         {
+            // check if request is from client connect and not from worldsave.
+            if (__result.Clients == null)
+                return;
+
+            var originalresult = __result;
+
             try
             {
-                if (cooldowns.ContainsKey(TimerId))
-                {
-                    var time = cooldowns[TimerId];
-                    var neededTime = time.AddSeconds(SaveClientsCooldown);
-                    if (neededTime > DateTime.Now)
-                    {
-                        // dont save new clients.
-                        return;
-                    }
-                    cooldowns.Remove(TimerId);
-                }
-                if (!cooldowns.ContainsKey(TimerId))
-                    cooldowns.Add(TimerId, DateTime.Now);
-
                 int count = 0;
                 __result.AllPlayersData.Dictionary = __result.AllPlayersData.Dictionary.Select(delegate (KeyValuePair<MyObjectBuilder_Checkpoint.PlayerId, MyObjectBuilder_Player> b)
                 {
@@ -53,11 +41,32 @@ namespace DePatch.GamePatches
                     key.ClientId = b.Key.GetClientId();
                     count++;
                     return new KeyValuePair<MyObjectBuilder_Checkpoint.PlayerId, MyObjectBuilder_Player>(key, b.Value);
-                }).ToDictionary((KeyValuePair<MyObjectBuilder_Checkpoint.PlayerId, MyObjectBuilder_Player> b) => b.Key, (KeyValuePair<MyObjectBuilder_Checkpoint.PlayerId, MyObjectBuilder_Player> b) => b.Value);
+
+                }).ToDictionary((KeyValuePair<MyObjectBuilder_Checkpoint.PlayerId, MyObjectBuilder_Player> b) => b.Key,
+                                (KeyValuePair<MyObjectBuilder_Checkpoint.PlayerId, MyObjectBuilder_Player> b) => b.Value);
                 if (count > 0)
+                {
+                    var TimerId = 123456789;
+
+                    if (cooldowns.ContainsKey(TimerId))
+                    {
+                        var time = cooldowns[TimerId];
+                        var neededTime = time.AddSeconds(5);
+                        if (neededTime > DateTime.Now)                           
+                            return; // dont spam log for many new clients.
+
+                        cooldowns.Remove(TimerId);
+                    }
+                    if (!cooldowns.ContainsKey(TimerId))
+                        cooldowns.Add(TimerId, DateTime.Now);
+
                     Log.Info($"Forced saving Client ids of {count} players");
-            } catch
+                }
+            }
+            catch
             {
+                __result = originalresult;
+                return;
             }
         }
     }
