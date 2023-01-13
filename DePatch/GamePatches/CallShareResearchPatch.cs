3ec7e9b0213027fd89f3e81a4c1eb412c691079b
Yuri <Yuri@bynet.co.il>
2023-01-13 17:40:36 +0300

    Addition to research share prevention code.
    When player in faction build new block he is sharing the research progress with all online faction members.
    So we block that, each player need to work on his progression research.
    
    This feature is offline by default.

--- DePatch/GamePatches/CallShareResearchPatch.cs
+++ DePatch/GamePatches/CallShareResearchPatch.cs
@@ -1,5 +1,8 @@
-﻿using Sandbox.Game.SessionComponents;
+﻿using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.SessionComponents;
 using Torch.Managers.PatchManager;
+using VRage.Game;
 
 namespace DePatch.GamePatches
 {
@@ -10,12 +13,29 @@ namespace DePatch.GamePatches
         public static void Patch(PatchContext ctx)
         {
             ctx.Prefix(typeof(MySessionComponentResearch), "CallShareResearch", typeof(CallShareResearchPatch), nameof(CallShareResearch));
+            ctx.Prefix(typeof(MySessionComponentResearch), "OnBlockBuilt", typeof(CallShareResearchPatch), nameof(OnBlockBuilt));
         }
 
         public static bool CallShareResearch(long toIdentity)
         {
             if (!DePatchPlugin.Instance.Config.Enabled || !DePatchPlugin.Instance.Config.BlockShareResearch)
                 return true;
+
+            return false;
+        }
+
+        private static bool OnBlockBuilt(MySessionComponentResearch __instance, MyCubeGrid grid, MySlimBlock block, ref bool handWelded)
+        {
+            if (!DePatchPlugin.Instance.Config.Enabled || !DePatchPlugin.Instance.Config.BlockShareResearch)
+                return true;
+
+            if (!handWelded)
+                return true;
+
+            long builtBy = block.BuiltBy;
+            MyDefinitionId id = block.BlockDefinition.Id;
+
+            __instance.UnlockResearch(builtBy, id, builtBy);
 
             return false;
         }
