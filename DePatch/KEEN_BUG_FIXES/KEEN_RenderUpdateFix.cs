7f76549402b44513c43ecc8808494db710683d02
Yuri <Yuri@bynet.co.il>
2023-04-30 17:30:06 +0300

    Fix KEEN Crash in thrusters RenderUpdate.

---
+++ DePatch/KEEN_BUG_FIXES/KEEN_RenderUpdateFix.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox.Game.Components;
+using Sandbox.Game.Entities;
+using Torch.Managers.PatchManager;
+using VRage.Game.Components;
+
+namespace DePatch.KEEN_BUG_FIXES
+{
+    [PatchShim]
+
+    public static class KEEN_RenderUpdateFix
+    {
+        public static void Patch(PatchContext ctx) => ctx.Prefix(typeof(MyThrust), typeof(KEEN_RenderUpdateFix), nameof(RenderUpdate));
+
+        private static bool RenderUpdate(MyThrust __instance)
+        {
+            if (!DePatchPlugin.Instance.Config.Enabled)
+                return true;
+
+            if (__instance.Render == null || __instance.Render.GetType() == typeof(MyNullRenderComponent) || __instance.Render.GetType() != typeof(MyRenderComponentThrust))
+                return false;
+            else
+                return true;
+        }
+    }
+}
