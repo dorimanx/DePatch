c9479ec8ac7a0ccc6ff2c696297877ed814e1a0a
Yuri <Yuri@bynet.co.il>
2024-06-15 20:08:25 +0300

    Fixed Weapons fire in PVE zone after update.

--- DePatch/PVEZONE/MyPVESafeZoneAction.cs
+++ DePatch/PVEZONE/MyPVESafeZoneAction.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using DePatch.CoolDown;
 using Sandbox.Game.Entities;
-using Sandbox.Game.Entities.Character;
 using Sandbox.Game.Multiplayer;
 using Sandbox.Game.Weapons;
 using Sandbox.Game.World;
@@ -26,7 +25,12 @@ namespace DePatch.PVEZONE
         public static bool BootTickStarted = true;
         private static bool ServerBootLoopStart = true;
 
-        public static void Patch(PatchContext ctx) => ctx.Prefix(typeof(MySessionComponentSafeZones), "IsActionAllowed", typeof(MyPVESafeZoneAction), nameof(IsActionAllowedPatch), new[] { "entity", "action", "sourceEntityId", "user" });
+        public static void Patch(PatchContext ctx)
+        {
+            ctx.Prefix(typeof(MySessionComponentSafeZones), "IsActionAllowed", typeof(MyPVESafeZoneAction), nameof(IsActionAllowedPatch1), new[] { "entity", "action", "sourceEntityId", "user" });
+            ctx.Prefix(typeof(MySessionComponentSafeZones), "IsActionAllowed", typeof(MyPVESafeZoneAction), nameof(IsActionAllowedPatch2), new[] { "aabb", "action", "sourceEntityId", "user" });
+            ctx.Prefix(typeof(MySessionComponentSafeZones), "IsActionAllowed", typeof(MyPVESafeZoneAction), nameof(IsActionAllowedPatch3), new[] { "point", "action", "sourceEntityId", "user" });
+        }
 
         private static bool CheckAllowedToLock(MyCubeGrid Grid)
         {
@@ -85,10 +89,7 @@ namespace DePatch.PVEZONE
             }
         }
 
-        private static bool IsActionAllowedPatch(
-#pragma warning disable IDE0060 // Remove unused parameter
-            MyEntity entity, MySafeZoneAction action, long sourceEntityId, ulong user, ref bool __result)
-#pragma warning restore IDE0060 // Remove unused parameter
+        private static bool IsActionAllowedPatch1(MyEntity entity, MySafeZoneAction action, long sourceEntityId, ulong user, ref bool __result)
         {
             if (!DePatchPlugin.Instance.Config.Enabled)
                 return true;
@@ -294,18 +295,93 @@ namespace DePatch.PVEZONE
                             if (!DePatchPlugin.Instance.Config.PveZoneEnabled)
                                 return true;
 
-                            if (PVE.CheckEntityInZone(entity, ref IsInPVEZone))
-                            {
-                                if (DePatchPlugin.Instance.Config.AllowToShootNPCinZone)
-                                    return true;
+                            if (DePatchPlugin.Instance.Config.AllowToShootNPCinZone)
+                                return true;
 
+                            if (PVE.CheckEntityInZone(entity, ref IsInPVEZone))
                                 return false;
-                            }
+
                             return true;
                         }
                 }
             }
+            return true;
+        }
 
+        public static bool IsActionAllowedPatch2(BoundingBoxD aabb, MySafeZoneAction action, long sourceEntityId, ulong user, ref bool __result)
+        {
+            if (!DePatchPlugin.Instance.Config.Enabled)
+                return true;
+
+            __result = false;
+
+            switch (action)
+            {
+                case MySafeZoneAction.Shooting:
+                    {
+                        if (DePatchPlugin.Instance.Config.DelayShootingOnBoot)
+                        {
+                            if (BootTickStarted)
+                            {
+                                // block weapons
+                                return false;
+                            }
+                        }
+
+                        if (!DePatchPlugin.Instance.Config.PveZoneEnabled)
+                            return true;
+
+                        if (DePatchPlugin.Instance.Config.AllowToShootNPCinZone)
+                            return true;
+
+                        BoundingBoxD Zone1 = PVE.PVESphere.GetBoundingBox();
+                        BoundingBoxD Zone2 = PVE.PVESphere2.GetBoundingBox();
+
+                        if (Zone1.Intersects(aabb) || (DePatchPlugin.Instance.Config.PveZoneEnabled2 && Zone2.Intersects(aabb)))
+                            return false;
+
+                        return true;
+                    }
+            }
+            return true;
+        }
+
+        public static bool IsActionAllowedPatch3(Vector3D point, MySafeZoneAction action, long sourceEntityId, ulong user, ref bool __result)
+        {
+            if (!DePatchPlugin.Instance.Config.Enabled)
+                return true;
+
+            __result = false;
+
+            switch (action)
+            {
+                case MySafeZoneAction.Shooting:
+                    {
+                        if (DePatchPlugin.Instance.Config.DelayShootingOnBoot)
+                        {
+                            if (BootTickStarted)
+                            {
+                                // block weapons
+                                return false;
+                            }
+                        }
+
+                        if (!DePatchPlugin.Instance.Config.PveZoneEnabled)
+                            return true;
+
+                        if (DePatchPlugin.Instance.Config.AllowToShootNPCinZone)
+                            return true;
+
+                        BoundingBoxD Zone1 = PVE.PVESphere.GetBoundingBox();
+                        BoundingBoxD Zone2 = PVE.PVESphere2.GetBoundingBox();
+                        BoundingBoxD boundingBox = new BoundingBoxD(point - 1.0, point + 1f);
+
+                        if (Zone1.Intersects(boundingBox) || (DePatchPlugin.Instance.Config.PveZoneEnabled2 && Zone2.Intersects(boundingBox)))
+                            return false;
+
+                        return true;
+                    }
+            }
             return true;
         }
     }
