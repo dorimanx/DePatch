64372806d8ea5df797cb74cad26eea3b35eb7442
Yuri.Sh <yuri@bynet.co.il>
2025-09-09 22:28:41 +0300

    Fixed PVP Radar detection code

--- DePatch/PVEZONE/PVE.cs
+++ DePatch/PVEZONE/PVE.cs
@@ -10,6 +10,8 @@ using VRageMath;
 using Torch.API.Managers;
 using DePatch.CoolDown;
 using Sandbox.ModAPI;
+using Sandbox.Game.Entities.Planet;
+using VRage.Game;
 
 namespace DePatch.PVEZONE
 {
@@ -19,6 +21,7 @@ namespace DePatch.PVEZONE
 
         public static List<long> EntitiesInZone = new List<long>();
         public static List<long> EntitiesInZone2 = new List<long>();
+        public static List<MyPlanet> ServerPlanets = new List<MyPlanet>();
         public static Dictionary<MyPlayer, string> PlanetScanner = new Dictionary<MyPlayer, string>();
         public static Dictionary<string, string> PlayerPlanet = new Dictionary<string, string>();
         private static int TickRadar = 1;
@@ -57,27 +60,98 @@ namespace DePatch.PVEZONE
             PVESphere = new BoundingSphereD(new Vector3D(plugin.Config.PveX, plugin.Config.PveY, plugin.Config.PveZ), plugin.Config.PveZoneRadius);
             PVESphere2 = new BoundingSphereD(new Vector3D(plugin.Config.PveX2, plugin.Config.PveY2, plugin.Config.PveZ2), plugin.Config.PveZoneRadius2);
 
-            PVPAlien = new BoundingSphereD(new Vector3D(5217892.6931551006, 3586960.28777134, 24330814.626217317), 160000);
-            PVPBarsoom = new BoundingSphereD(new Vector3D(-4487399.1877444545, 9924822.4033073764, -2047436.5433490407), 160000);
-            PVPCaldera = new BoundingSphereD(new Vector3D(4423873.9707971066, 3447997.6324882084, 24413094.900133859), 140000);
-            PVPDune = new BoundingSphereD(new Vector3D(24588174.166927494, -5100911.1766861351, 5471442.0334872119), 140000);
-            PVPEnigma = new BoundingSphereD(new Vector3D(48699129.693050854, 21283754.009524003, 7541964.1015719185), 140000);
-            PVPEuropa = new BoundingSphereD(new Vector3D(41137970.008399256, -28924780.373912029, 9528057.5614499226), 130000);
-            PVPFaraday = new BoundingSphereD(new Vector3D(3217874.6904728841, 4437198.9240576252, -980661.716303604), 160000);
-            PVPGalatea = new BoundingSphereD(new Vector3D(-4839914.8461530115, 9004595.1545535717, -4206229.5598180983), 160000);
-            PVPGea_Desert = new BoundingSphereD(new Vector3D(40830069.103547491, -29610859.878449582, 9242811.8954619151), 140000);
-            PVICE_Planet = new BoundingSphereD(new Vector3D(478468.08345142053, -886115.97622446285, -5028714.145565345), 140000);
-            PVPKaelnium = new BoundingSphereD(new Vector3D(4650948.8135129521, 3881444.7555122869, 24243070.801727097), 130000);
-            PVPNadrit = new BoundingSphereD(new Vector3D(2825437.2118936856, 4448763.2586455392, -872866.88817172288), 140000);
-            PVPOctave = new BoundingSphereD(new Vector3D(3989238.5409811782, 4793593.0767463958, -3009762.234743767), 160000);
-            PVPPertam = new BoundingSphereD(new Vector3D(1699744.1421741291, 1402014.6837624309, -41540.831866881112), 140000);
-            PVPSaturn = new BoundingSphereD(new Vector3D(-5075830.4638930392, 9433381.5605102256, -2903876.8130593989), 400000);
-            PVPSiberia = new BoundingSphereD(new Vector3D(49550725.786055669, 20648033.55527636, 7093292.6885949271), 160000);
-            PVPStyx = new BoundingSphereD(new Vector3D(-4960556.841415735, 9194154.8499497287, -4227386.9396974072), 160000);
-            PVPTerra = new BoundingSphereD(new Vector3D(49174670.003773756, 20716503.526764885, 7667419.7965744119), 160000);
-            PVPTitan = new BoundingSphereD(new Vector3D(4736792.882427997, 3720394.0052326508, 24437549.840063781), 140000);
-            PVPTriton = new BoundingSphereD(new Vector3D(41919144.845246159, -28762305.595447615, 10201790.434855942), 160000);
-            PVPValhalla = new BoundingSphereD(new Vector3D(25532376.310819753, 1877057.4612285423, 4580741.4395682905), 160000);
+            ServerPlanets = MyPlanets.GetPlanets();
+
+            foreach (var Planet in ServerPlanets)
+            {
+                if (Planet == null)
+                    continue;
+
+                if (Planet.Name == "Saturn")
+                {
+                    PVPSaturn = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 60000);
+                }
+                else if(Planet.Name == "PVP_Terra")
+                {
+                    PVPTerra = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Enigma_FreeLancers")
+                {
+                    PVPEnigma = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Pirates_Home")
+                {
+                    PVPKaelnium = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Siberia_FreeLancers")
+                {
+                    PVPSiberia = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Styx")
+                {
+                    PVPStyx = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Triton_Traders")
+                {
+                    PVPTriton = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Faradium_Planet")
+                {
+                    PVPFaraday = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Octave")
+                {
+                    PVPOctave = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Dune")
+                {
+                    PVPDune = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Titan_Pirates")
+                {
+                    PVPTitan = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_GalateaMoon")
+                {
+                    PVPGalatea = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_IcePlanet")
+                {
+                    PVICE_Planet = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Valhalla")
+                {
+                    PVPValhalla = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Barsoom")
+                {
+                    PVPBarsoom = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Gea_Desert_Traders")
+                {
+                    PVPGea_Desert = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Alien_Pirates")
+                {
+                    PVPAlien = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Europa_Traders")
+                {
+                    PVPEuropa = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Nadrid")
+                {
+                    PVPNadrit = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Caldera_Pirates")
+                {
+                    PVPCaldera = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+                else if (Planet.Name == "PVP_Pertam_Desert_Empire")
+                {
+                    PVPPertam = new BoundingSphereD(new Vector3D(Planet.PositionComp.GetPosition()), Planet.Provider.Radius + 40000);
+                }
+            }
 
             DamageHandler.Patch(ctx);
             Log.Info("Initing PVE ZONE... Complete!");
@@ -87,8 +161,8 @@ namespace DePatch.PVEZONE
         {
 #pragma warning disable 618
             TorchBase.Instance.CurrentSession.Managers.GetManager<IChatManagerServer>()
-#pragma warning restore 618
                 ?.SendMessageAsOther("PVP Radar", message, color == default ? Color.Gold : color, targetSteamId);
+#pragma warning restore 618
         }
 
         public static void AlertInPVPZone()
@@ -101,13 +175,13 @@ namespace DePatch.PVEZONE
 
             if (++TickRadar > 60)
             {
-                // loop for 180 sec and send PVP ping to chat.
+                // loop for 120 sec and send PVP ping to chat.
                 _ = CooldownManager.CheckCooldown(SteamIdCooldownKey.LoopRadarRequestID, null, out var remainingSecondsToNextRadarPing);
 
                 if (remainingSecondsToNextRadarPing < 1)
                 {
                     // arm new timer.
-                    int LoopCooldown = 60 * 1000;
+                    int LoopCooldown = 120 * 1000;
                     CooldownManager.StartCooldown(SteamIdCooldownKey.LoopRadarRequestID, null, LoopCooldown);
 
                     var OnlinePlayersList = MySession.Static.Players.GetOnlinePlayers().ToList();
@@ -231,34 +305,56 @@ namespace DePatch.PVEZONE
 
                     foreach (var Player in PlanetScanner)
                     {
+                        if (Player.Key == null)
+                            continue;
+
                         var gravityVectorTemp = 0.0f;
                         Vector3D position = Player.Key.GetPosition();
                         var gravityVector = MyAPIGateway.Physics.CalculateNaturalGravityAt(position, out gravityVectorTemp);
-                        var GridGravityCalc = gravityVector.Length() / EARTH_GRAVITY;
+                        var PlayerGravityCalc = gravityVector.Length() / EARTH_GRAVITY;
+                        var PlanetGravity = 0.15f;
 
                         if (!PlayerPlanet.ContainsKey(Player.Key.DisplayName))
                         {
-                            if (!MySession.Static.IsUserAdmin(myPlayer.Id.SteamId))
+                            if (!MySession.Static.IsUserAdmin(Player.Key.Id.SteamId))
                             {
-                                if (GridGravityCalc > 0.4f)
+                                MyPlanet closestPlanet = MyPlanets.Static.GetClosestPlanet(Player.Key.Character.PositionComp.GetPosition());
+
+                                if (closestPlanet != null)
                                 {
-                                    SendMessage($"{Player.Key.DisplayName} arrived to planet/прибыл на планету {Player.Value}");
-                                    PlayerPlanet.Add(Player.Key.DisplayName, Player.Value);
+                                    var PlanetObject = closestPlanet.GetObjectBuilder();
+                                    MyObjectBuilder_Planet ThisPlanetConfig = (MyObjectBuilder_Planet)PlanetObject;
+                                    float PlanetGrav = ThisPlanetConfig.SurfaceGravity;
+
+                                    if (PlayerGravityCalc >= PlanetGrav || PlayerGravityCalc >= PlanetGrav - 0.15f)
+                                    {
+                                        SendMessage($"{Player.Key.DisplayName} arrived to planet/прибыл на планету {Player.Value}");
+                                        PlayerPlanet.Add(Player.Key.DisplayName, Player.Value);
+                                    }
+                                }
+                                else
+                                {
+                                    if (PlayerGravityCalc >= PlanetGravity)
+                                    {
+                                        SendMessage($"{Player.Key.DisplayName} arrived to planet/прибыл на планету {Player.Value}");
+                                        PlayerPlanet.Add(Player.Key.DisplayName, Player.Value);
+                                    }
                                 }
                             }
                         }
                         else
                         {
-                            PlayerPlanet.TryGetValue(myPlayer.DisplayName, out string PlayerNameOnPlanet);
-                            PlanetScanner.TryGetValue(myPlayer, out string PlayerNameDetectedPlanet);
+                            PlayerPlanet.TryGetValue(Player.Key.DisplayName, out string PlayerNameOnPlanet);
+                            PlanetScanner.TryGetValue(Player.Key, out string PlayerNameDetectedPlanet);
 
                             if (PlayerNameOnPlanet == PlayerNameDetectedPlanet)
                                 return;
                             else
                             {
                                 SendMessage($"{Player.Key.DisplayName} left planet/покинул планету {PlayerNameOnPlanet}");
+                                PlayerPlanet.Remove(Player.Key.DisplayName);
                                 SendMessage($"{Player.Key.DisplayName} arrived to planet/прибыл на планету {Player.Value}");
-                                PlayerPlanet.Remove(myPlayer.DisplayName);
+                                PlayerPlanet.Add(Player.Key.DisplayName, PlayerNameDetectedPlanet);
                             }
                         }
                     }
